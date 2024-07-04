b6a259081ec3450eb5ff0b0c3e8e36288c4cd760
Yashraj Singh <89309972+yash-raj2000@users.noreply.github.com>
2024-07-04 12:42:47 +0530

    Updated Form.jsx
    added spinner for form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Box from "@mui/material/Box";
+import LinearProgress from "@mui/material/LinearProgress";
 
 function Form(props) {
   const [inputs, setInputs] = useState({
@@ -11,12 +13,15 @@ function Form(props) {
     message: "",
   });
 
+  const [spinner, setSpinner] = useState(false);
+
   function handleChange(e) {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setSpinner(true);
     try {
       const result = await axios.post(
         "https://myportfolio-8hq0.onrender.com/addMessage",
@@ -24,6 +29,7 @@ function Form(props) {
       ); //This server is rendering on render
       console.log(result);
       if (result.status === 200) {
+        setSpinner(false);
         // or whatever status indicates success
         setInputs({
           fName: "",
@@ -107,6 +113,14 @@ function Form(props) {
           <button className="form-btn send" type="submit">
             SEND
           </button>
+          {spinner && (
+            <Box
+              sx={{ width: "100%" }}
+              style={{ width: "70%", margin: "20px" }}
+            >
+              <LinearProgress />
+            </Box>
+          )}
         </div>
       </form>
     </div>
